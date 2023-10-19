9ce21c9edd7ef0b2344b19da479a2b33701b944f
Eunji <bian87@dgu.ac.kr>
2023-10-19 17:05:26 +0900

    design: MovieContent css 제거, 스타일드 컴포넌트로 변경

--- src/component/Movie/MovieContent.jsx
+++ src/component/Movie/MovieContent.jsx
@@ -1,17 +1,49 @@
-import './MovieContent.css';
+import styled from 'styled-components';
 
+const MoviecontentCard = styled.div`
+  display: block;
+  position: absolute;
+  max-width: 150px;
+  max-height: 300px; 
+  background-color: rgba(1, 1, 1, 0.7);
+  z-index: 500;
+`
+
+const ContentIntroduction = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+  height: 300px;
+  overflow-y: auto; 
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const MoviecontentTitle = styled.h2`
+  margin: 30px 10px 20px 10px;
+  font-size: 10px;
+  color: #EFF4F5
+`
+
+const MoviecontentContent = styled.div`
+  margin: 0px 10px 0px 10px;
+  font-size: 10px;
+  color: #EFF4F5
+`
 
 function MovieContent({movieContentData}) {
 
   const {title, overview} = movieContentData;
   
     return (
-        <div className="moviecontent-card">
-        <div className="content-introduction">
-        <h2 className="moviecontent-title">{title}</h2> 
-        <div className="moviecontent-content">{overview}</div>
-        </div>
-        </div>
+        <MoviecontentCard>
+        <ContentIntroduction>
+        <MoviecontentTitle>{title}</MoviecontentTitle> 
+        <MoviecontentContent>{overview}</MoviecontentContent>
+        </ContentIntroduction>
+        </MoviecontentCard>
     )
   }
   
